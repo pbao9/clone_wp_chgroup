4a99927d5b7dd28f0789e098af2e90a092427448
Pham Bao <baobaoo5533@gmail.com>
2023-11-08 10:11:08 +0700

    first commit

---
+++ wp-content/plugins/content-views-query-and-display-post-page/block/build/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'react-dom', 'wp-api-fetch', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-dom-ready', 'wp-element', 'wp-i18n', 'wp-server-side-render'), 'version' => '40f2a9b75b93f714c2fe');
